e676587e14076322b78f2ec4d89037f004f55607
Ivanka Fromel <125952478+IVANKA1901@users.noreply.github.com>
2023-08-08 21:50:51 +0200

    Delete dist directory

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/", function (req, res) {
-    res.send("Hello world");
-});
-app.listen(4000, () => {
-    console.log(`Server running. Use our API on port: 4000`);
-});
-//# sourceMappingURL=app.js.map
