44ee5d760b31bb5cb5c354d8b4792cdd12793e2c
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-08-12 21:57:00 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
 const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+
+const contactsRouter = require("./routes/api/contacts-routes");
+const authRouter = require("./routes/api/auth-routes");
+
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello world");
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
 });
 
-app.listen(4000, () => {
-  console.log(`Server running. Use our API on port: 4000`);
+app.use("*", (req, res) => {
+  res.status(404).json("Route not found");
 });
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server Error" } = err;
+  res.status(status).json({ message });
+});
+
+module.exports = app;
