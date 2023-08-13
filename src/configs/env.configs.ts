a77eafad3774e596bbbb77f2297be9d4579e5d71
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-08-13 13:58:33 +0200

    configs

--- src/configs/env.configs.ts
+++ src/configs/env.configs.ts
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const envConfig = {
-  DB_HOST: process.env.DB_HOST,
+  DB_HOST: process.env.DB_HOST ?? "",
   PORT: process.env.PORT || 3000,
 };
