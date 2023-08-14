5f3c81513a3ca432d68142e0f7e30bdc7fe05a1b
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-08-14 11:41:15 +0200

    add routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,1 +1,13 @@
-// import express from "express";
+import express from "express";
+
+import { notesControllers } from "../controllers/api/notes.js";
+
+const router = express.Router();
+
+router.get("/", notesControllers.getAllNotes);
+router.get("/:id", notesControllers.getNoteById);
+router.post("/", notesControllers.saveNote);
+router.put("/:id", notesControllers.updateNote);
+router.delete("/:id", notesControllers.deleteNote);
+
+export default router;
