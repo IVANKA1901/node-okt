7ee8d1f2ebee894ded72f35fcd7ee906376f10fb
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-08-14 18:09:15 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,41 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import logger from "morgan";
 import cors from "cors";
+
+import { RequestError } from "./interfaces/request-error.interface";
+import notesRouter from "./routes/api";
 
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const bodyParser = require("body-parser");
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.static("public"));
+
+app.use("/api/notes", notesRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json("Route not found");
 });
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server Error" } = err;
-  res.status(status).json({ message });
+app.use((req: Request, res: Response): void => {
+  res.status(404).json({ message: "Not found" });
 });
+
+app.use(
+  (
+    err: RequestError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): void => {
+    const { status = 500, message = "Server Error" } = err;
+    res.status(status).json({ message });
+  }
+);
 
 export default app;
